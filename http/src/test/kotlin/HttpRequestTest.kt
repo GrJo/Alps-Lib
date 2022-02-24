5d6e72aa7c469962d58f752f1e67ec2ecc843bad
LordTuxn <r3tuxn@gmail.com>
2022-02-24 19:24:41 +0100

    improve tests

--- http/src/test/kotlin/HttpRequestTest.kt
+++ http/src/test/kotlin/HttpRequestTest.kt
@@ -1,26 +1,38 @@
 import com.alpsbte.alpslib.htpp2.HttpRequest
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 class HttpRequestTest {
     companion object {
         @JvmStatic
-        fun httpRequests(): Stream<String> {
+        fun httpRequestsWithoutAuth(): Stream<String> {
             return Stream.of(
-                "https://alps-bte.com/plotapi/v1/get_plots")
+                "https://api.opentopodata.org/v1/test-dataset?locations=56,123")
         }
     }
 
     @ParameterizedTest
-    @MethodSource("httpRequests")
+    @MethodSource("httpRequestsWithoutAuth")
     fun testHttpGet(request: String) {
         val response : HttpRequest.HttpResponse = HttpRequest.getJSON(request, null)
         assertEquals(200, response.responseCode)
 
         assertNotNull(response.jsonElement)
-        //val obj: JsonObject? = response.jsonElement?.asJsonObject
+        assertNull(response.jsonObject)
+    }
+
+    @Test
+    fun testHttpGetWithAuth() {
+        val response : HttpRequest.HttpResponse = HttpRequest.getJSON("https://alps-bte.com/plotapi/v1/get_plots",
+            null, "API_KEY")
+        assertEquals(200, response.responseCode)
+
+        assertNotNull(response.jsonElement)
+        assertNull(response.jsonObject)
     }
 }
